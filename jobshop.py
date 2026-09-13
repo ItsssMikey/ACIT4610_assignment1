73d1d8da4793db5915c4aa8d0be3fb3c5c765985
noaho04 <noah@ofsti.com>
2026-09-13 14:18:18 +0200

    removed unnecessary complexity in regards to having another class. consolidated all into a GA class, which takes a problem instance as constructor + args

--- jobshop.py
+++ jobshop.py
@@ -1,34 +1,23 @@
 import random
 
-class JSSP:
-    def __init__(self, num_jobs, num_machines, jobs):
-        self.num_jobs = num_jobs
-        self.num_machines = num_machines
-        self.jobs = jobs
-
-    @classmethod
-    def from_file(cls, filepath):
-        with open(filepath, "r") as file:
-            lines = file.readlines()
-        
-        num_jobs, num_machines = map(int, lines[0].split())
-    
-        jobs = []
-    
-        for line in lines[1:]:
-            values = list(map(int, line.split()))
-    
-            operations = []
-    
-            for i in range(0, len(values), 2):
-                machine = values[i]
-                processing_time = values[i+1]
-    
-                operations.append((machine, processing_time))
-    
-            jobs.append(operations)
-        
-        return cls(num_jobs, num_machines, jobs)
+class GeneticAlgorithm:
+    def __init__(
+        self,
+        instance,
+        population_size = 100,
+        generations     = 1000,
+        mutation_rate   = 0.2,
+        crossover_rate  = 0.8,
+        tournament_size = 2,
+    ):
+        self.num_jobs         = instance[0]
+        self.num_machines     = instance[1]
+        self.jobs             = instance[2]
+        self.population_size  = population_size
+        self.generations      = generations
+        self.mutation_rate    = mutation_rate
+        self.crossover_rate   = crossover_rate
+        self.tournament_size  = tournament_size
 
 
     def create_chromosome(self):
@@ -124,38 +113,19 @@ class JSSP:
         return schedule, makespan
 
 
-
-class GeneticAlgorithm:
-    def __init__(
-        self,
-        instance,
-        population_size = 100,
-        generations = 1000,
-        mutation_rate = 0.2,
-        crossover_rate = 0.8,
-        tournament_size = 2,
-    ):
-        self.instance = instance
-        self.population_size = population_size
-        self.generations = generations
-        self.mutation_rate = mutation_rate
-        self.crossover_rate = crossover_rate
-        self.tournament_size = tournament_size
-
-
     def create_population(self):
         '''
         Create an initial population of chromosomes for the job shop scheduling problem.
         Each chromosome is a random permutation of job IDs, where each job ID appears as many times as it has operations (equal to the number of machines).
         '''
         population = [
-            self.instance.create_chromosome()
+            self.create_chromosome()
             for _ in range(self.population_size)
         ]
         
         population_fitness_list = []
         for chromosome in population:
-            fitness_value = self.instance.fitness(chromosome)
+            fitness_value = self.fitness(chromosome)
             population_fitness_list.append([chromosome, fitness_value])
 
         return population_fitness_list
@@ -176,7 +146,7 @@ class GeneticAlgorithm:
         child1 = [None] * list_len
         child2 = [None] * list_len
 
-        selected = set(random.sample(range(self.instance.num_jobs), self.instance.num_jobs // 2))
+        selected = set(random.sample(range(self.num_jobs), self.num_jobs // 2))
 
         remaining_for_child1 = []
         for job in parent2:
@@ -206,6 +176,7 @@ class GeneticAlgorithm:
 
         return child1, child2
 
+
     def mutate(self, chromosome):
         '''
         Perform mutation on a chromosome by swapping two random genes.
@@ -217,6 +188,7 @@ class GeneticAlgorithm:
         mutant[index1], mutant[index2] = mutant[index2], mutant[index1]
 
         return mutant
+
 
     def run(self):
         population_fitness_list = self.create_population()
@@ -239,7 +211,7 @@ class GeneticAlgorithm:
 
             while len(new_population) < self.population_size:
                 parent1 = self.tournament_selection(population_fitness_list)
-                parent2 = self.tournament_selection(population_fitness_list,)
+                parent2 = self.tournament_selection(population_fitness_list)
 
                 parent_chromosome1 = parent1[0]
                 parent_chromosome2 = parent2[0]
@@ -263,8 +235,8 @@ class GeneticAlgorithm:
                 if random.random() < self.mutation_rate:
                     child_chromosome2 = self.mutate(child_chromosome2)
 
-                child1 = [child_chromosome1, self.instance.fitness(child_chromosome1)]
-                child2 = [child_chromosome2, self.instance.fitness(child_chromosome2)]
+                child1 = [child_chromosome1, self.fitness(child_chromosome1)]
+                child2 = [child_chromosome2, self.fitness(child_chromosome2)]
 
                 # Add children to new population
                 new_population.append(child1)
@@ -283,14 +255,38 @@ class GeneticAlgorithm:
         return best_entry
 
 
+def load_data(filepath):
+    with open(filepath, "r") as file:
+        lines = file.readlines()
+    
+    num_jobs, num_machines = map(int, lines[0].split())
+
+    jobs = []
+
+    for line in lines[1:]:
+        values = list(map(int, line.split()))
+
+        operations = []
+
+        for i in range(0, len(values), 2):
+            machine = values[i]
+            processing_time = values[i+1]
+
+            operations.append((machine, processing_time))
+
+        jobs.append(operations)
+    
+    return [num_jobs, num_machines, jobs]
+
+
 if __name__ == "__main__":
-    instance = JSSP.from_file("./data/la01.txt")
-    algorithm = GeneticAlgorithm(instance)
+    instance = load_data("./data/la01.txt")
+    jssp_solver = GeneticAlgorithm(instance)
 
-    best_solution = algorithm.run()
+    best_solution = jssp_solver.run()
 
-    print("Best solution found:", best_solution[0])
-    schedule, makespan = algorithm.instance.build_schedule(best_solution[0])
+    print("Best solution found:", best_solution)
+    schedule, makespan = jssp_solver.build_schedule(best_solution[0])
     
     print("\nSchedule:")
     for operation in schedule:
